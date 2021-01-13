5da80b1d12d48247b744cb3cf459981c85a1967f
Jessy <jessybreyne@protonmail.com>
2021-01-13 19:04:13 +0100

    Connexion réussie

--- src/base.js
+++ src/base.js
@@ -2,13 +2,20 @@ import Rebase from 're-base'
 import firebase from 'firebase/app'
 import 'firebase/database'
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyD3ZZJWhRcfguQAh4LnyFm_-tI10xX0V5Y",
-    authDomain: "chatbox-app-de271.firebaseapp.com",
-    databaseURL: "https://chatbox-app-de271-default-rtdb.europe-west1.firebasedatabase.app"
-})
 
-const base = Rebase.createClass(firebase.database())
+const config = {
+    apiKey: "AIzaSyCoGcUF08abbwDOl8AhLb6L64k7O96QvJI",
+    authDomain: "chatbox-app-a87ed.firebaseapp.com",
+    databaseURL: "https://chatbox-app-a87ed-default-rtdb.firebaseio.com",
+    projectId: "chatbox-app-a87ed",
+    storageBucket: "chatbox-app-a87ed.appspot.com",
+    messagingSenderId: "1092483073402",
+    appId: "1:1092483073402:web:8a0be25dffcd84c912c2c3"
+}
+
+const firebaseApp = firebase.initializeApp(config)
+
+const base = Rebase.createClass(firebaseApp.database())
 
 export { firebaseApp }
 
